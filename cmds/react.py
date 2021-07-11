0800d219be91cdb19857df9a7449a7eb3764071d
Tony <tony32059613@gmail.com>
2021-07-12 01:10:28 +0800

    1.複誦訊息 2.清除多則訊息

--- cmds/react.py
+++ cmds/react.py
@@ -25,5 +25,14 @@ class React(Cog_Extension):
 		random_pic = random.choice(jsonData['url_pic'])
 		await ctx.send(random_pic)
 
+	@commands.command()
+	async def sayd(self,ctx, *,msg):
+		await ctx.message.delete()
+		await ctx.send(msg)
+
+	@commands.command()
+	async def clean(self, ctx, num:int):
+		await ctx.channel.purge(limit=num+1)
+
 def setup(bot):
 	bot.add_cog(React(bot))
