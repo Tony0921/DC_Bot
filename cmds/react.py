76584ff00b3fb5887c6ac03b0bb09154567ff982
Tony <tony32059613@gmail.com>
2021-07-12 02:57:55 +0800

    embed內嵌文字

--- cmds/react.py
+++ cmds/react.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 from core.classes import Cog_Extension
 import random
 import json
+import datetime
+import pytz
+
+TWtime = pytz.timezone('Asia/Taipei')
 
 with open('setting.json', mode='r', encoding='utf8') as jsonFile:
 	jsonData = json.load(jsonFile)
@@ -33,6 +37,19 @@ class React(Cog_Extension):
 	@commands.command()
 	async def clean(self, ctx, num:int):
 		await ctx.channel.purge(limit=num+1)
+
+	@commands.command()
+	async def cmd(self,ctx):
+		embed=discord.Embed(title="Bot Commands", color=0xfbff00,timestamp=TWtime.localize(datetime.datetime.now()))
+		embed.set_author(name="Xioa_Yu Bot")
+		embed.set_thumbnail(url="https://icon-library.com/images/command-line-512.png")
+		embed.add_field(name="!pic", value="特定圖片", inline=False)
+		embed.add_field(name="!rand_pic", value="圖庫隨機圖片", inline=False)
+		embed.add_field(name="!url_pic", value="網址圖庫隨機圖片", inline=False)
+		embed.add_field(name="!sayd [you want to say]", value="機器人幫你說", inline=False)
+		embed.add_field(name="!clean [num]", value="清除n筆訊息", inline=False)
+		embed.set_footer(text="Made by Tony Chen")
+		await ctx.send(embed=embed)
 
 def setup(bot):
 	bot.add_cog(React(bot))
