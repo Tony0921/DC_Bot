538847eebcc23dde199aa30a6ccfbb9732710513
Tony <tony32059613@gmail.com>
2021-07-14 02:18:51 +0800

    1.加入圖片庫 2.加入新增、刪除、查看圖片 3.移除隨機圖片 4.修改cmd命令

--- cmds/react.py
+++ cmds/react.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands.core import command
 from core.classes import Cog_Extension
 import random
 import json
@@ -14,22 +15,6 @@ with open('setting.json', mode='r', encoding='utf8') as jsonFile:
 class React(Cog_Extension):
 
 	@commands.command()
-	async def pic(self,ctx):
-		pic = discord.File(jsonData['capoo_pic'][0])
-		await ctx.send(file=pic)
-
-	@commands.command()
-	async def rand_pic(self,ctx):
-		random_pic = random.choice(jsonData['capoo_pic'])
-		pic = discord.File(random_pic)
-		await ctx.send(file=pic)
-
-	@commands.command()
-	async def url_pic(self,ctx):
-		random_pic = random.choice(jsonData['url_pic'])
-		await ctx.send(random_pic)
-
-	@commands.command()
 	async def sayd(self,ctx, *,msg):
 		await ctx.message.delete()
 		await ctx.send(msg)
@@ -39,13 +24,65 @@ class React(Cog_Extension):
 		await ctx.channel.purge(limit=num+1)
 
 	@commands.command()
+	async def pic(self, ctx, name:str):
+		source = jsonData['pic_lib']
+		with open(source, mode='r', encoding='utf8') as rf:
+			data = json.load(rf)
+		try:
+			await ctx.send(data[name])
+		except KeyError:
+			await ctx.send(f'{name} is not in library!')
+
+	@commands.command()
+	async def add_pic(self, ctx , name:str, url:str):
+		source = jsonData['pic_lib']
+		if url.startswith('https://'):
+			with open(source, mode='r', encoding='utf8') as rf:
+				data = json.load(rf)
+			data[name] = url
+			with open(source, 'w', encoding='utf8') as wf:
+				json.dump(data, wf)
+			await ctx.send("Add success!")
+		else:
+			await ctx.send("Unsuccessed!")
+
+	@commands.command()
+	async def rmv_pic(self, ctx, name:str):
+		source = jsonData['pic_lib']
+		with open(source, mode='r', encoding='utf8') as rf:
+			data = json.load(rf)
+		try:
+			del data[name]
+			with open(source, 'w', encoding='utf8') as wf:
+				json.dump(data, wf)
+			await ctx.send("Remove successed!")
+		except KeyError:
+			await ctx.send(f'{name} is not in library!')
+
+	@commands.command()
+	async def pic_lib(self, ctx):
+		source = jsonData['pic_lib']
+		with open(source, mode='r', encoding='utf8') as rf:
+			data = json.load(rf)
+		embed=discord.Embed(title="圖片庫", color=0x00d9ff,timestamp=TWtime.localize(datetime.datetime.now()))
+		embed.set_author(name="小煜 Bot")
+		embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Picture_icon_BLACK.svg/1200px-Picture_icon_BLACK.svg.png")
+		for k, v in data.items():
+			embed.add_field(name=k, value=v, inline=False)
+		embed.set_footer(text="Made by Tony Chen")
+		await ctx.send(embed=embed)
+
+	@commands.command()
 	async def cmd(self,ctx):
 		embed=discord.Embed(title="Bot Commands", color=0xfbff00,timestamp=TWtime.localize(datetime.datetime.now()))
-		embed.set_author(name="Xioa_Yu Bot")
+		embed.set_author(name="小煜 Bot")
 		embed.set_thumbnail(url="https://icon-library.com/images/command-line-512.png")
-		embed.add_field(name="!pic", value="特定圖片", inline=False)
-		embed.add_field(name="!rand_pic", value="圖庫隨機圖片", inline=False)
-		embed.add_field(name="!url_pic", value="網址圖庫隨機圖片", inline=False)
+		embed.add_field(name="!pic [name]", value="URL圖庫圖片", inline=False)
+		embed.add_field(name="!add_pic [name] [URL]", value="新增URL圖片", inline=False)
+		embed.add_field(name="!rmv_pic [name]", value="刪除URL圖片", inline=False)
+		embed.add_field(name="!pic_lib", value="查看圖庫", inline=False)
+		# embed.add_field(name="!rand_pic", value="圖庫隨機圖片", inline=False)
+		# embed.add_field(name="!url_pic", value="網址圖庫隨機圖片", inline=False)
 		embed.add_field(name="!sayd [you want to say]", value="機器人幫你說", inline=False)
 		embed.add_field(name="!clean [num]", value="清除n筆訊息", inline=False)
 		embed.set_footer(text="Made by Tony Chen")
