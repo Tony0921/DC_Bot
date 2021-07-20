553ba8643d0e3f7f57b47d5e840dc065e45fc238
Tony <tony32059613@gmail.com>
2021-07-20 17:48:35 +0800

    新增自動取得移除身分組功能

--- cmds/event.py
+++ cmds/event.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import cog
 from core.classes import Cog_Extension
 import json
 
@@ -18,7 +19,34 @@ class Event(Cog_Extension):
 		print(f'{member}leave!')
 		channel = self.bot.get_channel(int(jsonData['Main_channel']))
 		await channel.send(f'{member}leave!')
-	
+
+	@commands.Cog.listener()
+	async def on_raw_reaction_add(self,info):
+		source = jsonData['roles']
+		with open(source, mode='r', encoding='utf8') as rf:
+			data = json.load(rf)
+		if info.message_id == 866975390589255690:
+			for k, v in data.items():
+				if str(info.emoji) == k:
+					guild = self.bot.get_guild(info.guild_id)
+					role = guild.get_role(int(v))
+					await info.member.add_roles(role)
+					await info.member.send(f"你在 {guild.name} 取得了 {role} 身分組!")
+
+	@commands.Cog.listener()
+	async def on_raw_reaction_remove(self, info):
+		source = jsonData['roles']
+		with open(source, mode='r', encoding='utf8') as rf:
+			data = json.load(rf)
+		if info.message_id == 866975390589255690:
+			for k, v in data.items():
+				if str(info.emoji) == k:
+					guild = self.bot.get_guild(info.guild_id)
+					user = guild.get_member(info.user_id)
+					role = guild.get_role(int(v))
+					await user.remove_roles(role)
+					await user.send(f"你在 {guild.name} 移除了 {role} 身分組!")
+
 	@commands.Cog.listener()
 	async def on_message(self, msg):
 		if msg.content == '...':
