8d470b2d8947e3d07558d6755c4634ed9fe462c6
MY-HOME-PC\Tony <tony32059613@gmail.com>
2022-09-23 19:25:28 +0800

    加入錯誤紀錄功能

--- cmds/err_event.py
+++ cmds/err_event.py
@@ -2,9 +2,17 @@ import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
 
+import time
+
 class Err_event(Cog_Extension):
 	@commands.Cog.listener()
 	async def on_command_error(self, ctx, error):
+		#error log
+		t = time.localtime()
+		result = time.strftime("%Y/%m/%d %H:%M:%S ", t)
+		f = open("./private_files/err_log.txt", "a")
+		f.write(result + str(error) + "\n")
+		f.close()
 		
 		if hasattr(ctx.command, 'on_error'): #若有自己的錯誤處理就跳過
 			return
