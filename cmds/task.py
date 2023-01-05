5d809c9795a29cf7451381cbdfa3d1ea41a68579
MY-HOME-PC\Tony <tony32059613@gmail.com>
2023-01-05 21:34:03 +0800

    1.分離Class Name 2.更改getLiveTime Json檔結構

--- cmds/task.py
+++ cmds/task.py
@@ -54,7 +54,7 @@ def getLiveStatus(live_info):
 async def getLiveTime():
 	# 設定chrome
 	options = Options()
-	options.add_argument("--headless")
+	options.add_argument("--headless") #背景執行，不彈窗
 	options.add_argument("--disable-notifications")
 	options.add_argument("--no-sandbox")
 	options.add_argument("User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
@@ -70,8 +70,16 @@ async def getLiveTime():
 		chrome.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 		await asyncio.sleep(3)
 
+	#顯示更多按鈕的Class Name
+	# show_moreBtnClassName = 'x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f'
+
+	getLiveTimeJson = jsonData['getLiveTime']
+	with open(getLiveTimeJson, 'r', encoding='utf8') as rf:
+		datas = json.load(rf)
+	ClassName = datas['HTMLClassName']
+
 	scripts = "\
-	var elements = document.getElementsByClassName('qi72231t nu7423ey n3hqoq4p r86q59rh b3qcqh3k fq87ekyn bdao358l fsf7x5fv rse6dlih s5oniofx m8h3af8h l7ghb35v kjdc1dyq kmwttqpk srn514ro oxkhqvkx rl78xhln nch0832m cr00lzj9 rn8ck1ys s3jn8y49 icdlwmnq cxfqmxzd pbevjfx6 innypi6y');\
+	var elements = document.getElementsByClassName('" + ClassName['show_moreBtn'] + "');\
 	for(let i=0;i < elements.length;i++){\
 		if(elements[i].innerHTML=='顯示更多'){\
 			console.log(elements[i].innerHTML);\
@@ -84,7 +92,8 @@ async def getLiveTime():
 
 	# HTML解析
 	root = bs4.BeautifulSoup(chrome.page_source, "html.parser")
-	str1 = 'gvxzyvdx aeinzg81 t7p7dqev gh25dzvf exr7barw b6ax4al1 gem102v4 ncib64c9 mrvwc6qr sx8pxkcf f597kf1v cpcgwwas m2nijcs8 hxfwr5lz k1z55t6l oog5qr5w tes86rjd pbevjfx6 ztn2w49o'
+	# str1 = 'x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h'
+	str1 = ClassName['targetClassName']
 	targets = root.find_all('span',{'class':str1})
 
 	total_text = ""
@@ -145,9 +154,10 @@ class Task(Cog_Extension):
 		now_m = now_time.minute
 		now_wd = now_time.weekday()+1
 		print(now_time)
-		timeSetting = jsonData['getLiveTime']
-		with open(timeSetting, 'r', encoding='utf8') as rf:
-			_time = json.load(rf)
+		getLiveTimeJson = jsonData['getLiveTime']
+		with open(getLiveTimeJson, 'r', encoding='utf8') as rf:
+			datas = json.load(rf)
+		_time = datas['time']
 		if now_wd==_time['wd'] and now_h==_time['h'] and now_m==_time['m']:
 			result = await getLiveTime()
 			print(result)
